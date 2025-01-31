27b2f3cce8dd16ced024f245c3e4ff5e2227fc63
Kaan Çolak <kaancolak95@gmail.com>
2025-01-31 12:39:08 +0300

    pass topics

--- launch/pointcloud_densifier.launch.py
+++ launch/pointcloud_densifier.launch.py
@@ -7,18 +7,32 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     pkg_prefix = get_package_share_directory('pointcloud_densifier')
-    
     config_file = os.path.join(pkg_prefix, 'config/pointcloud_densifier.param.yaml')
 
+    # Declare arguments
+    input_topic_arg = DeclareLaunchArgument(
+        'input_topic',
+        default_value='/sensing/lidar/concatenated/pointcloud',
+        description='Input pointcloud topic'
+    )
+    
+    output_topic_arg = DeclareLaunchArgument(
+        'output_topic',
+        default_value='/perception/pointcloud_densifier/pointcloud',
+        description='Output pointcloud topic'
+    )
+
     return LaunchDescription([
+        input_topic_arg,
+        output_topic_arg,
         Node(
             package='pointcloud_densifier',
             executable='pointcloud_densifier_node',
             name='pointcloud_densifier',
             parameters=[config_file],
             remappings=[
-                ('input', '/sensing/lidar/concatenated/pointcloud'),
-                ('output', '/perception/pointcloud_densifier/pointcloud')
+                ('input', LaunchConfiguration('input_topic')),
+                ('output', LaunchConfiguration('output_topic'))
             ],
         )
     ])
